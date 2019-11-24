2b298a5529a4b7b63e278e672077a6717ad0f20a
Rodolfo Lam <r_lam@hotmail.com>
2019-11-23 20:18:35 -0500

    Use standard conventions for header inclusion.
    Libary file should use the <*> idiom. Project files should use the "*" idiom.

--- LucarIO/LucarIO.h
+++ LucarIO/LucarIO.h
@@ -9,10 +9,10 @@
 #include <string>
 #include <vector>
 
-#include "allegro5/allegro.h"
-#include "allegro5/allegro_font.h"
-#include "allegro5/allegro_image.h"
-#include "allegro5/allegro_primitives.h"
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_primitives.h>
 
 #include "Player.h"
 #include "Screen.h"
