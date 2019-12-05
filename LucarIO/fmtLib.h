cea2e458fe6b4e0e755099ae38aae76652547699
Rodolfo Lam <r_lam@hotmail.com>
2019-12-05 01:23:00 -0500

    Fix binary compatability
    {fmt} was built with the incorrect configuration for the proyect. Now we have both a Debug and a Release version. Previously we only had a Release version present.

---
+++ LucarIO/fmtLib.h
@@ -0,0 +1,10 @@
+/*
+	LucarIO - Un intento de Agar.IO para molestar a Fernando
+	04/DIC/2019 - Copyright (C) 2019 Rodolfo Lam. All rights reserved.
+*/
+#pragma once
+
+#include <fmt/core.h>
+#include <fmt/format.h>
+#include <fmt/format-inl.h>
+#include <../src/format.cc>
